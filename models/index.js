85022b3bc2a8a1b9656c16af2b1a2f8fcef0a271
Tre Scott <tres1396@gmail.com>
2021-04-25 14:58:40 -0400

    editing routes

--- models/index.js
+++ models/index.js
@@ -5,13 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: "category_name" //this could be wrong?? FIXME
+})
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: "product_name" //this could be wrong?? FIXME
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  foreignKey: "tag_name" //this could be wrong?? FIXME
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  foreignKey: "product_name" //this could be wrong?? FIXME
+})
 module.exports = {
   Product,
   Category,
