141b84a5d77524ddf0a6d33b0f431bd54125dff6
Tre Scott <tres1396@gmail.com>
2021-04-25 16:03:49 -0400

    added post route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -21,13 +21,38 @@ router.get('/', (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+       // be sure to include its associated Products
+      include: [
+        {
+          model: Product,
+          attributes: ["id", "product_name", "price", "stock", "category_id"]
+        },
+      ],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({message: "No category found with that id"})
+      return;
+    }
+
+    res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
