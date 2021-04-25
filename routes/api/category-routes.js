cdcdb1a09875fcf631da1ad7cbc5371c401dee67
Tre Scott <tres1396@gmail.com>
2021-04-25 15:53:05 -0400

    added route to find all categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,19 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [
+        {
+          model: Product,
+          attributes: ["id", "product_name", "price", "stock", "category_id"]
+        },
+      ],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
