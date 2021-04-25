1b4b5e2ee1a861227651324be216f7aa7db650a4
Tre Scott <tres1396@gmail.com>
2021-04-25 16:42:49 -0400

    fixing typos in delete route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -79,14 +79,14 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
-  Category.destroy({
+  Tag.destroy({
     where: {
       id: req.params.id,
     }
   })
   .then((tagData) => {
     if (!tagData) {
-      res.status(404).json({message: "No category found with that id"});
+      res.status(404).json({message: "No tag found with that id"});
       return;
     }
     res.json(tagData);
