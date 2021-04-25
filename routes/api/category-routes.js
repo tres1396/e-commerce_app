bd7a7e8b0e34745d0a20eef365af94c2d8a81b46
Tre Scott <tres1396@gmail.com>
2021-04-25 16:10:43 -0400

    added put route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -57,6 +57,23 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+})
+
+.then ((categoryData) => {
+  if (!categoryData) {
+    res.status(400).json({message: "No category found with that id"})
+    return;
+  }
+  res.json(categoryData);
+})
+.catch((err) => {
+  console.log(err);
+  res.status(500).json(err);
+});
 });
 
 router.delete('/:id', (req, res) => {
